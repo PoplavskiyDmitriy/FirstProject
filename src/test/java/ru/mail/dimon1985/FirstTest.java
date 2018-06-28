b5d2f2f4685a8aa33694b4f6538343db44256004
PoplavskiyDmitriy <dimon-198505@mail.ru>
2018-06-28 23:19:14 +0300

    renamed variables

--- src/test/java/ru/mail/dimon1985/FirstTest.java
+++ src/test/java/ru/mail/dimon1985/FirstTest.java
@@ -11,6 +11,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileWriter;
 import java.math.BigDecimal;
@@ -21,8 +25,8 @@ import java.util.concurrent.TimeUnit;
 public class FirstTest {
     private static WebDriver driver;
     private static int counter = 0;
-    private static Double whole_amount=0.0;
-    private static Double whole_amount_abolished=0.0;
+    private static Double sumTotal=0.0;
+    private static Double canceledSumTotal=0.0;
     private static double total;
 
     @BeforeClass
@@ -36,65 +40,58 @@ public class FirstTest {
     }
     @Test
     public void tasktest() {
+        final Wait<WebDriver> wait = new WebDriverWait(driver,10).withMessage("Element not found");
         WebElement boxClose = driver.findElement(By.xpath("//a[text()='Спасибо, я уже открыл']"));
         boxClose.click();
         driver.navigate().refresh();
-        WebElement fz_223_radiobtn = driver.findElement(By.id("dnn_ctr691_View_RadioButton9"));
-        fz_223_radiobtn.click();
+        WebElement radioButton = driver.findElement(By.id("dnn_ctr691_View_RadioButton9"));
+        radioButton.click();
         ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(0));
         driver.close();
         driver.switchTo().window(tabs.get(1));
-        WebElement fz_223_checkbox = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_chkPurchaseType_0"));
-        fz_223_checkbox.sendKeys(Keys.SPACE);
-        WebElement commercial_purchase = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_chkPurchaseType_1"));
-        commercial_purchase.click();
+        WebElement fz223ChecBbox = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_chkPurchaseType_0"));
+        fz223ChecBbox.sendKeys(Keys.SPACE);
+        WebElement commercialPurchase = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_chkPurchaseType_1"));
+        commercialPurchase.click();
         WebElement startPrice = driver.findElement(By.id("BaseMainContent_MainContent_txtStartPrice_txtRangeFrom"));
         startPrice.click();
         startPrice.sendKeys("0");
-        WebElement search_button = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_btnSearch"));
-        if (fz_223_checkbox.isSelected()&&commercial_purchase.isSelected()){
-            search_button.click();
+        WebElement searchButton = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_btnSearch"));
+        if (fz223ChecBbox.isSelected()&&commercialPurchase.isSelected()){
+            searchButton.click();
         }
-        WebElement next = driver.findElement(By.cssSelector("#next_t_BaseMainContent_MainContent_jqgTrade_toppager"));
+        WebElement nextPage = driver.findElement(By.cssSelector("#next_t_BaseMainContent_MainContent_jqgTrade_toppager"));
         String price;
-        String eis;
-        String price_value;
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WebElement page_count = driver.findElement(By.xpath("//span[@id=\"sp_1_BaseMainContent_MainContent_jqgTrade_toppager\"]"));
-       String pages1 = page_count.getText().toString();
-        pages1 = pages1.replaceAll(" ","").trim();
-       Integer k = Integer.valueOf(pages1);
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WebElement current_page = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_jqgTrade_toppager_center > table > tbody > tr > td:nth-child(4) > input"));
-        String page_current = current_page.getAttribute("value");
-        Integer l = Integer.valueOf(page_current.trim());
-        List<WebElement> eis_number;
-        List<WebElement> start_price;
-        WebElement cell;
-        WebElement cell_price;
+        String eisNumberValue;
+        String priceValue;
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[@id=\"sp_1_BaseMainContent_MainContent_jqgTrade_toppager\"]"))));
+        WebElement pageCounter = driver.findElement(By.xpath("//span[@id=\"sp_1_BaseMainContent_MainContent_jqgTrade_toppager\"]"));
+       String totalPages = pageCounter.getText().toString();
+        totalPages = totalPages.replaceAll(" ","").trim();
+       Integer totalPagesValue = Integer.valueOf(totalPages);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#BaseMainContent_MainContent_jqgTrade_toppager_center > table > tbody > tr > td:nth-child(4) > input"))));
+        WebElement currentPage = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_jqgTrade_toppager_center > table > tbody > tr > td:nth-child(4) > input"));
+        String currentPageAttribute = currentPage.getAttribute("value");
+        Integer currentPageValue = Integer.valueOf(currentPageAttribute.trim());
+        List<WebElement> eisNumberColumn;
+        List<WebElement> startPriceColumn;
+        WebElement EisCell;
+        WebElement PriceCell;
       while (true){
-          if (l<k) {
-              eis_number = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_OosNumber']"));
-              start_price = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_StartPrice']"));
-              for (int i = 0; i < eis_number.size(); i++) {
-                  cell = eis_number.get(i);
-                  eis = cell.getText();
-                  if (!eis.trim().equals("")) {
-                      cell_price = start_price.get(i);
-                      price = cell_price.getText();
-                      price_value = price.substring(0, price.length() - 4);
-                      price_value = price_value.replaceAll(" ", "");
-                      price_value = price_value.replaceAll(",", ".");
-                      whole_amount = Double.valueOf(price_value) + whole_amount;
+          if (currentPageValue<totalPagesValue) {
+              eisNumberColumn = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_OosNumber']"));
+              startPriceColumn = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_StartPrice']"));
+              for (int i = 0; i < eisNumberColumn.size(); i++) {
+                  EisCell = eisNumberColumn.get(i);
+                  eisNumberValue = EisCell.getText();
+                  if (!eisNumberValue.trim().equals("")) {
+                      PriceCell = startPriceColumn.get(i);
+                      price = PriceCell.getText();
+                      priceValue = price.substring(0, price.length() - 4);
+                      priceValue = priceValue.replaceAll(" ", "");
+                      priceValue = priceValue.replaceAll(",", ".");
+                      sumTotal = Double.valueOf(priceValue) + sumTotal;
                       counter = counter + 1;
                   }
               }
@@ -103,56 +100,53 @@ public class FirstTest {
               } catch (InterruptedException e) {
                   e.printStackTrace();
               }
-              next.click();
+              nextPage.click();
               try {
                   TimeUnit.SECONDS.sleep(2);
               } catch (InterruptedException e) {
                   e.printStackTrace();
               }
-              eis_number = null;
-              start_price = null;
+
+              eisNumberColumn = null;
+              startPriceColumn = null;
           }
           else { break;}
-          try {
-              TimeUnit.SECONDS.sleep(3);
-          } catch (InterruptedException e) {
-              e.printStackTrace();
+          currentPageAttribute = currentPage.getAttribute("value");
+          currentPageValue = Integer.valueOf(currentPageAttribute.trim());
           }
-          page_current = current_page.getAttribute("value");
-          l = Integer.valueOf(page_current.trim());
-          }
-        WebElement abolished = driver.findElement(By.cssSelector("#lotStateTabs > ul > li:nth-child(10) > a"));
-        abolished.click();
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WebElement page_count_abolished = driver.findElement(By.xpath("//*[@id=\"sp_1_BaseMainContent_MainContent_jqgTrade_toppager\"]"));
-        WebElement current_page_abolished = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_jqgTrade_toppager_center > table > tbody > tr > td:nth-child(4) > input"));
-        WebElement next_abolished = driver.findElement(By.cssSelector("#next_t_BaseMainContent_MainContent_jqgTrade_toppager"));
-        pages1 = null;
-        pages1 = page_count_abolished.getText().toString();
-        pages1 = pages1.replaceAll(" ","").trim();
-        k = Integer.valueOf(pages1);
-        page_current = current_page_abolished.getAttribute("value");
-        l = Integer.valueOf(page_current.trim());
-        int counter_abolished=0;
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#lotStateTabs > ul > li:nth-child(10) > a"))));
+        WebElement canceledButton = driver.findElement(By.cssSelector("#lotStateTabs > ul > li:nth-child(10) > a"));
+        canceledButton.click();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[@id=\"sp_1_BaseMainContent_MainContent_jqgTrade_toppager\"]"))));
+        WebElement canceledPageCount = driver.findElement(By.xpath("//*[@id=\"sp_1_BaseMainContent_MainContent_jqgTrade_toppager\"]"));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#BaseMainContent_MainContent_jqgTrade_toppager_center > table > tbody > tr > td:nth-child(4) > input"))));
+        WebElement canceledCurrentPage = driver.findElement(By.cssSelector("#BaseMainContent_MainContent_jqgTrade_toppager_center > table > tbody > tr > td:nth-child(4) > input"));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("#next_t_BaseMainContent_MainContent_jqgTrade_toppager"))));
+        WebElement canceledNextPage = driver.findElement(By.cssSelector("#next_t_BaseMainContent_MainContent_jqgTrade_toppager"));
+        totalPages = null;
+        totalPages = canceledPageCount.getText().toString();
+        totalPages = totalPages.replaceAll(" ","").trim();
+        totalPagesValue = Integer.valueOf(totalPages);
+        currentPageAttribute = canceledCurrentPage.getAttribute("value");
+        currentPageValue = Integer.valueOf(currentPageAttribute.trim());
+        int canceledCounter=0;
         while (true){
-            if (l<k) {
-                eis_number = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_OosNumber']"));
-                start_price = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_StartPrice']"));
-                for (int i = 0; i < eis_number.size(); i++) {
-                    cell = eis_number.get(i);
-                    eis = cell.getText();
-                    if (!eis.trim().equals("")) {
-                        cell_price = start_price.get(i);
-                        price = cell_price.getText();
-                        price_value = price.substring(0, price.length() - 4);
-                        price_value = price_value.replaceAll(" ", "");
-                        price_value = price_value.replaceAll(",", ".");
-                        whole_amount_abolished = Double.valueOf(price_value) + whole_amount_abolished;
-                        counter_abolished = counter_abolished + 1;
+            if (currentPageValue<totalPagesValue) {
+                wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_OosNumber']"))));
+                eisNumberColumn = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_OosNumber']"));
+                wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_StartPrice']"))));
+                startPriceColumn = driver.findElements(By.xpath("//td[@aria-describedby='BaseMainContent_MainContent_jqgTrade_StartPrice']"));
+                for (int i = 0; i < eisNumberColumn.size(); i++) {
+                    EisCell = eisNumberColumn.get(i);
+                    eisNumberValue = EisCell.getText();
+                    if (!eisNumberValue.trim().equals("")) {
+                        PriceCell = startPriceColumn.get(i);
+                        price = PriceCell.getText();
+                        priceValue = price.substring(0, price.length() - 4);
+                        priceValue = priceValue.replaceAll(" ", "");
+                        priceValue = priceValue.replaceAll(",", ".");
+                        canceledSumTotal = Double.valueOf(priceValue) + canceledSumTotal;
+                        canceledCounter = canceledCounter + 1;
                     }
                 }
                 try {
@@ -160,31 +154,26 @@ public class FirstTest {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                next_abolished.click();
+                canceledNextPage.click();
                 try {
                     TimeUnit.SECONDS.sleep(2);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                eis_number = null;
-                start_price = null;
+                eisNumberColumn = null;
+                startPriceColumn = null;
             }
             else { break;}
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            page_current = current_page_abolished.getAttribute("value");
-            l = Integer.valueOf(page_current.trim());
+            currentPageAttribute = canceledCurrentPage.getAttribute("value");
+            currentPageValue = Integer.valueOf(currentPageAttribute.trim());
         }
         System.out.println("Total number of lots: "+counter);
         System.out.print("Total start price: ");
-        System.out.printf("%.2f",whole_amount);
+        System.out.printf("%.2f",sumTotal);
         System.out.println();
         System.out.print("Total start price of canceled lots: ");
-        System.out.printf("%.2f",whole_amount_abolished);
-        total = whole_amount-whole_amount_abolished;
+        System.out.printf("%.2f",canceledSumTotal);
+        total = sumTotal-canceledSumTotal;
         System.out.println();
         System.out.print("Total: ");
         System.out.printf("%.2f",total);
@@ -195,9 +184,9 @@ public class FirstTest {
         FileWriter report = new FileWriter("F://report.txt");
         report.write("Total number of lots: "+counter+"\n");
         report.write("Total start price: ");
-        report.write(String.format("%(.2f",whole_amount));
+        report.write(String.format("%(.2f",sumTotal));
         report.write("\nTotal start price of canceled lots: ");
-        report.write(String.format("%(.2f",whole_amount_abolished));
+        report.write(String.format("%(.2f",canceledSumTotal));
         report.write("\nTotal: ");
         report.write(String.format("%(.2f",total));
         report.close();
